d3253ff9a6645c8a3ff3de8a6a8f7f297b887da8
Hyeonmo <khm052678@gmail.com>
2022-09-20 16:17:51 +0900

    #39 - 시큐리티 설정과 자바 대응 코드 구현
    스프링 부트 2.7 과 가이드에 맞춰서
    시큐리티 설정을 적용하고, 인증 정보를 db로부터 부르는 빈과
    패스워드 인코더 빈까지 등록
    영향 받는 부분을 서비스 코드와 컨트롤러에 반영

--- src/main/java/com/board/projectboard/config/SecurityConfig.java
+++ src/main/java/com/board/projectboard/config/SecurityConfig.java
@@ -1,19 +1,60 @@
 package com.board.projectboard.config;
 
+import com.board.projectboard.dto.UserAccountDto;
+import com.board.projectboard.dto.security.BoardPrincipal;
+import com.board.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+    @Bean                                                   // security 5.7 ~ 부터 해야하는 방식
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"  // 이 것들에 대해 인증이 되어야함
+                        ).permitAll()
+                        .anyRequest().authenticated()       // 룰 적용
+                )
                 .formLogin().and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
                 .build();
+    }
+
+//    @Bean                                                   // Warning 문구로 이 방법은 더이상 추천하지 않아 제외
+//    public WebSecurityCustomizer webSecurityCustomizer() {  // 이 부분 등록 내용은 security 검사 제외
+//        // static resource, css - js
+//        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
